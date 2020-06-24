177c714f591764bb8b8d185664ac1bede0da9016
TtH61008 <imaseki1008@gmail.com>
2020-06-24 19:42:10 +0900

    devデータの追加

--- src/preprocess.py
+++ src/preprocess.py
@@ -39,7 +39,9 @@ result_df["label"] = result_df["label"].astype("int")
 
 random_sorted_df = result_df.sample(frac=1)
 
-split_len = int(len(random_sorted_df)*0.8)
+train_split_len = int(len(random_sorted_df)*0.6)
+dev_split_len = int(len(random_sorted_df)*0.8)
 
-random_sorted_df[:split_len].to_csv(args.output_dir+"/train.tsv", sep="\t", header=None,index=None)
-random_sorted_df[split_len:].to_csv(args.output_dir+"/test.tsv", sep="\t", header=None,index=None)
+random_sorted_df[:train_split_len].to_csv(args.output_dir+"/train.tsv", sep="\t", header=None,index=None)
+random_sorted_df[train_split_len:dev_split_len].to_csv(args.output_dir+"/dev.tsv", sep="\t", header=None,index=None)
+random_sorted_df[dev_split_len:].to_csv(args.output_dir+"/test.tsv", sep="\t", header=None,index=None)
